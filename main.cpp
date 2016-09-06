774092f5140ee0adc7a1ffec2c59d68d9008bcfb
Daniel Ponce de León <dannzero@gmail.com>
2016-09-05 19:34:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+/*	Blah blah
+ *	blah blah blah
+*/
+
 #include<iostream>
 #include<stack>
+#include<queue>
 #include<string>
+using namespace std;
 
 int main(){
+	stack pila;
+	queue cola;
+	string s;
+	//Input Stream al string
+	cin >> s;
+	
+	
 	//TO DO code here...
 }
